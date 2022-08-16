352ff00c39605c68976d7f967434d145c143d480
cerasusflorere <cerasusflorere.180@gmail.com>
2022-08-16 20:28:53 +0900

    1-諸々設定終わり

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: 'dist/',
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' webpack 1 用
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        
+        loader: 'vue-loader'
+      }
+    ]
+  }
+}
